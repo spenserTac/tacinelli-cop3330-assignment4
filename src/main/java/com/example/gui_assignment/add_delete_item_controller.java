1e100bcdc8223017d1a9e643663365d9423d5caa
spenserTac <spenserdt@gmail.com>
2021-10-21 23:55:08 -0400

    first commit

---
+++ src/main/java/com/example/gui_assignment/add_delete_item_controller.java
@@ -0,0 +1,21 @@
+package com.example.gui_assignment;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class add_delete_item_controller {
+
+    @FXML
+    private AnchorPane item;
+
+    @FXML
+    void add_item(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete_item(ActionEvent event) {
+
+    }
+}
